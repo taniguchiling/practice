857696c584b666edadd0c63b2d93021581c341de
Yudai INOUE <>
2018-03-23 14:11:43 +0900

    ちょっと修正

--- rental2.py
+++ rental2.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-# import pandas as pd
-
 import csv
+# import pandas as pd
 
 
 csv_file_path = ""
 
 
+# csvデータのパスを指定して読み込む準備をするための関数
 def import_csv_data():
     global v
     global csv_file_path
@@ -17,6 +17,7 @@ def import_csv_data():
     # df = pd.read_csv(csv_file_path)
 
 
+# csvを開いて表示するための関数
 def open_csv_data():
     with open(csv_file_path, newline="") as file:
         reader = csv.reader(file)
@@ -40,3 +41,4 @@ tk.Button(root, text='開く', command=open_csv_data).grid(row=1, column=3)
 root.mainloop()
 
 # エントリ（テキストボックス）形式で各セルに情報を表示して書き換え可能にして、それを列・行ごとにまるまるcsvファイルに上書きすれば書き換えできるかも
+# リストボックスで表示したほうがスマートな感じ
